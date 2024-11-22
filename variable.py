25a7f5cfb3573a2f9fa051ed261bce24bc205d7f
Danil <koz.love.90@gmail.com>
2024-11-22 11:28:13 +0300

    Update variable.py

--- variable.py
+++ variable.py
@@ -3,4 +3,4 @@ compl_hw = 12
 hours = 1.5
 course = 'Python'
 task_time = hours / compl_hw
-print(f'Курс: {course}, всего задач:{compl_hw}, затрачено часов: {hours}, среднее время выполнения {task_time}')
+print(f'Курс: {course}, всего задач:{compl_hw}, затрачено часов: {hours}, среднее время выполнения {task_time} часа.')
