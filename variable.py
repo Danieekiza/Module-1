60432ce43d12612870904d3f34f5680f9c4bb909
Danil <koz.love.90@gmail.com>
2024-11-22 11:17:58 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,6 @@
+# variables
+compl_hw = 12
+hours = 1.5
+course = 'Python'
+task_time = hours / compl_hw
+print(f'Курс: {course}, всего задач:{compl_hw}, затрачено часов: {hours}, среднее время выполнения {task_time}')
