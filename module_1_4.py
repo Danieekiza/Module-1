c07653e1651339629c6d7c4e8046ee93f31d0060
Danil <koz.love.90@gmail.com>
2024-11-25 14:54:32 +0300

    Организация программ и методы строк

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+# module_1_4.py
+my_string = input()
+print('Колличество введенных символов:', len(my_string))
+print('Вывод в верхнем регистре:', my_string.upper())
+print('Вывод в нижнем регистре:', my_string.lower())
+print('Вывод без пробелов:', my_string.replace(' ', '',))
+print('Первый символ строки:',my_string[0])
+print('Последний символ строки:',my_string[-1])
