f6df85d996ce6e681f853234f68db7a19fc34edf
Danil <koz.love.90@gmail.com>
2024-11-26 17:25:25 +0300

    Список и кортеж

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+# list and tuple
+immutable_var = 1, 2, 3, 'apple', ['a' , 'b' , 'c']
+print('кортеж:', immutable_var)
+# immutable_var[0] = 4 # кортеж - неизменяемая коллекция
+mutable_list = [1,2,3,4,5]
+print('список:', mutable_list)
+mutable_list [0] = 'abc'
+print('список измененный:', mutable_list)
